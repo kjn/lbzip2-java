7c978f312d6ba4f71db17a3b74e9915bd79577c1
Mikolaj Izdebski <zurgunt@gmail.com>
2014-01-11 17:55:50 +0100

    Add JUnite test case stub

---
+++ src/lb2xp/TestMBC.java
@@ -0,0 +1,14 @@
+package lb2xp;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class TestMBC
+{
+    @Test
+    public void testExpand()
+    {
+        fail( "Not yet implemented" );
+    }
+}
