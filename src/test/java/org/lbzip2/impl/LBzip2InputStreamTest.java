fe64621ad3e966d9b7ec6a1ebfc20dee930c8dc4
Mikolaj Izdebski <zurgunt@gmail.com>
2014-01-17 21:32:25 +0100

    Add unit test for LBzip2InputStream

---
+++ src/test/java/org/lbzip2/impl/LBzip2InputStreamTest.java
@@ -0,0 +1,44 @@
+/*-
+ * Copyright (c) 2014 Mikolaj Izdebski
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.lbzip2.impl;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.junit.Test;
+
+/**
+ * @author Mikolaj Izdebski
+ */
+public class LBzip2InputStreamTest
+{
+    @Test
+    public void test()
+        throws Exception
+    {
+        InputStream fis = new FileInputStream( "test-data/concat.bz2" );
+        LBzip2InputStream zis = new LBzip2InputStream( fis );
+
+        assertEquals( 'T', zis.read() );
+        assertEquals( 'E', zis.read() );
+        assertEquals( 'S', zis.read() );
+        assertEquals( 'T', zis.read() );
+        assertEquals( '\n', zis.read() );
+        assertEquals( -1, zis.read() );
+    }
+}
